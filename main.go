490108a4759314b16a23a5a482b11d4b1b21efde
sankalp_j <sankalp@greedygame.com>
2019-02-28 18:09:56 +0530

    formatted

--- main.go
+++ main.go
@@ -1,44 +1,44 @@
 package main
 
 import (
-  "os"
-  "os/signal"
-  "syscall"
-  "time"
-  "net/http"
-  "log"
-  "fmt"
-  "strings"
-  "context"
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+	"time"
 
-  "github.com/gin-gonic/gin"
-  "github.com/sankalpjonn/ecount"
-  "database/sql"
-  _ "github.com/mattn/go-sqlite3"
+	"database/sql"
+	"github.com/gin-gonic/gin"
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/sankalpjonn/ecount"
 )
 
 func beforeEvictHook(db *sql.DB) func(map[string]int) {
-    return func(eventCntMap map[string]int) {
-      query := "INSERT INTO chat_click_event(shop_id, hour, count) values(?, ?, ?)"
-      for k, v := range eventCntMap {
-        log.Println("got query: ", query)
-        insert, err := db.Prepare(query)
-      	if err != nil {
-          panic(err)
-        }
+	return func(eventCntMap map[string]int) {
+		query := "INSERT INTO chat_click_event(shop_id, hour, count) values(?, ?, ?)"
+		for k, v := range eventCntMap {
+			log.Println("got query: ", query)
+			insert, err := db.Prepare(query)
+			if err != nil {
+				panic(err)
+			}
 
-        _, err = insert.Exec(strings.Split(k, "|")[0], strings.Split(k, "|")[1], v)
-      	if err != nil {
-      		panic(err)
-      	}
-        insert.Close()
-      }
-    }
+			_, err = insert.Exec(strings.Split(k, "|")[0], strings.Split(k, "|")[1], v)
+			if err != nil {
+				panic(err)
+			}
+			insert.Close()
+		}
+	}
 }
 
 func handler(ec ecount.Ecount) gin.HandlerFunc {
-  fn := func(ginContext *gin.Context) {
-    ec.Incr(fmt.Sprintf("%s|%s", ginContext.Param("event"), time.Now().Format("2006010215")))
+	fn := func(ginContext *gin.Context) {
+		ec.Incr(fmt.Sprintf("%s|%s", ginContext.Param("event"), time.Now().Format("2006010215")))
 		ginContext.Header("content-type", "application/json;charset=utf-8")
 		ginContext.JSON(http.StatusOK, gin.H{
 			"status": "ok",
@@ -48,20 +48,20 @@ func handler(ec ecount.Ecount) gin.HandlerFunc {
 }
 
 func main() {
-  db, err := sql.Open("sqlite3", "/home/sankalpjonna/gowork/src/github.com/sankalpjonn/events/test.db")
-  if err != nil {
-    panic(err)
-  }
-  defer db.Close()
+	db, err := sql.Open("sqlite3", "/home/sankalpjonna/gowork/src/github.com/sankalpjonn/events/test.db")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
 
-  ec := ecount.New(time.Second * 60, beforeEvictHook(db))
-  defer ec.Stop()
+	ec := ecount.New(time.Second*60, beforeEvictHook(db))
+	defer ec.Stop()
 
-  r := gin.New()
-  r.Use(gin.Logger())
-  r.GET("/events/:event", handler(ec))
+	r := gin.New()
+	r.Use(gin.Logger())
+	r.GET("/events/:event", handler(ec))
 
-  srv := &http.Server{
+	srv := &http.Server{
 		Addr:    ":8080",
 		Handler: r,
 	}
@@ -70,17 +70,17 @@ func main() {
 			log.Fatalf("listen: %s\n", err)
 		}
 	}()
-  defer func(){
-    ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-  	defer cancel()
-  	if err := srv.Shutdown(ctx); err != nil {
-  		log.Fatal("Server Shutdown:", err)
-  	} else {
-      log.Println("gracefully shut down server")
-    }
-  }()
+	defer func() {
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		if err := srv.Shutdown(ctx); err != nil {
+			log.Fatal("Server Shutdown:", err)
+		} else {
+			log.Println("gracefully shut down server")
+		}
+	}()
 
-  sigc := make(chan os.Signal, 1)
+	sigc := make(chan os.Signal, 1)
 	signal.Notify(sigc,
 		syscall.SIGHUP,
 		syscall.SIGINT,
