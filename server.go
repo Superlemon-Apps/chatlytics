8644f35dad797a14dd3d78fac25635e2a2412273
sankalp_j <sankalp@greedygame.com>
2019-03-07 11:10:35 +0530

    added cors middleware

--- server.go
+++ server.go
@@ -38,9 +38,26 @@ func handler(ec ecount.Ecount) gin.HandlerFunc {
 	return gin.HandlerFunc(fn)
 }
 
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
+
 func getServer(ec ecount.Ecount) *http.Server {
 	r := gin.New()
 	r.Use(gin.Logger())
+	r.Use(CORSMiddleware())
 	r.GET("/chatlytics/chat", handler(ec))
 
 	// start server
